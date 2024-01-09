6450659ea0c19134a2b754d5829fb2db839a5df8
Abdelhadi Chajia <abdelhadi-chajia@outlook.fr>
2024-01-09 12:22:27 -0800

    function that writes a string to a text file (UTF8) and returns the number of characters written:

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""This module defines a text file-reading function"""
+"""This module defines a file-writing function."""
 
 
 def write_file(filename="", text=""):
-    """Prints the contents of a UTF8 text file"""
-    with open(filename,"w", encoding="utf-8") as f:
+    """Writes a string to a UTF8 text file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
